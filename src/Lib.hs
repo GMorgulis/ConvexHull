e1134e95c31233e7bb324369b84c9c0bd3ee9668
George Morgulis <george055204@gmail.com>
2024-11-21 19:57:24 -0500

    minor bug fixes

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,25 +1,9 @@
 module Lib
-    ( quickhull
+    ( someFunc
     ) where
 
-import Data.List (maximumBy, minimumBy)
-import Data.Ord (comparing)
-
-quickhull :: IO ()
-quickhull = do
+someFunc :: IO ()
+someFunc = do
     print ("Hello World")
 
 
-type C2 = (Double, Double)
-
-{-Computes the maximum of points by specified dimension-}
-maxd :: [C2] -> Int -> C2
-maxd points 0 = maximumBy (comparing fst) points
-maxd points 1 = maximumBy (comparing snd) points
-maxd _ _ = error "Invalid arguements."
-
-{-Computes the minimum of points by specified dimension-}
-mind :: [C2] -> Int -> C2
-mind points 0 = minimumBy (comparing fst) points
-mind points 1 = minimumBy (comparing snd) points
-mind _ _ = error "Invalid arguements."
