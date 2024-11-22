fba28c15cdbcd1d1429403f6d91b4983cc396113
George Morgulis <george055204@gmail.com>
2024-11-21 23:31:47 -0500

    minor bug fixes

--- src/qhseq.hs
+++ src/qhseq.hs
@@ -32,29 +32,29 @@ mind _ _ = error "Invalid arguements."
 
 {-Calculates the maximum area given a line segment-}
 maxArea :: C2 -> C2 -> [C2] -> C2
-maxArea anchor1 anchor2 points = maximumBy (comparing(\p -> triArea anchor1 anchor2 p)) points
+maxArea anchor1 anchor2 = maximumBy (comparing (triArea anchor1 anchor2))
 
 {-Determines whether a point lies to the right or left of a line segment-}
 grouper :: C2 -> C2 -> [C2] -> ([C2],[C2])
 grouper anchor1 anchor2 points = helper anchor1 anchor2 points [] []
-    where 
+    where
         helper _ _ [] group1 group2 = (group1, group2)
-        helper (x1, y1) (x2, y2) (z:zs) group1 group2 
+        helper (x1, y1) (x2, y2) (z:zs) group1 group2
             |  (x2 - x1) * ((snd z) - y1) - (y2 - y1) * (fst z - x1) > 0 = helper (x1, y1) (x2, y2) zs (z : group1) group2
             | otherwise = helper (x1, y1) (x2, y2) zs group1 (z : group2)
- 
+
 
 {-Keeps all points outside the triangle. Works for everything other than points on triangle itself-}
 keepOuter :: C2 -> C2 -> C2 -> [C2] -> [C2]
 keepOuter t1 t2 t3 points = helper t1 t2 t3 points []
-    where 
+    where
         helper _ _ _ [] keep = keep
-        helper p1 p2 p3 (x : xs) keep 
+        helper p1 p2 p3 (x : xs) keep
             | pointInTriangle p1 p2 p3 x = helper p1 p2 p3 xs keep
-            | otherwise = helper p1 p2 p3 xs (x : keep) 
+            | otherwise = helper p1 p2 p3 xs (x : keep)
 
 {-Finds the area of a triangle-}
-triArea :: C2 -> C2 -> C2 -> Double 
+triArea :: C2 -> C2 -> C2 -> Double
 triArea (x1, y1) (x2, y2) (x3, y3) = (0.5) * abs ((x1 * (y2 - y3)) + (x2 * (y3 - y1)) + (x3 * (y1 - y2)))
 
 {-Checks if point is in triangle-}
