91407aa7cc1d7d67bb4c5b3bd60dc4b175a224b4
George Morgulis <gm3138@columbia.edu>
2024-12-13 17:04:13 -0500

    major bug fixes in alg

--- src/QuickHullV.hs
+++ src/QuickHullV.hs
@@ -12,7 +12,6 @@ import Data.Ord (comparing)
 import qualified Data.Vector.Unboxed as V
 import Control.Parallel (par, pseq)
 
---Control.Parallel.Strategies (parList, rseq, using)
 
 
 {-New vector type-}
@@ -25,25 +24,22 @@ quickh points d = V.cons a1 (V.cons a2 hpar)
   where
     a1 = minv points
     a2 = maxv points
-    groups = grouper a1 a2 points 
-    h1 = ph (fst groups) a1 a2 d
-    h2 = ph (snd groups) a2 a1 d
-    hpar =  h1 `par` (h2 `pseq` (h1 V.++ h2))
+    h1 = ph points a1 a2 (depthUpdate d)
+    h2 = ph points a2 a1 (depthUpdate d) -- note the diffrence in order
+    hpar =  h1 `par` (h2 `pseq` V.concat [h1, h2]) 
 
-ph :: VV2 -> V2 -> V2 -> Int -> VV2 
-ph points a1 a2 d 
-  | V.null points = V.empty 
-  | d > 0 = V.cons m1 hpar
+ph :: VV2 -> V2 -> V2 -> Int -> VV2
+ph points a1 a2 d
+  | V.length group == 0 = V.empty 
+  | d > 0 && V.length group > 250000 = V.cons m1 hpar
   | otherwise = V.cons m1 h
-  where 
-    group = fst (grouper a1 a2 points)
+  where
+    group = grouper a1 a2 points
     m1 = maxAreaPoint a1 a2 group
-    h1 = ph group a1 m1 (depthUpdate d) 
-    h2 = ph group m1 a2 (depthUpdate d) 
-    h = h1 V.++ h2
-    hpar = h1 `par` (h2 `pseq` (h1 V.++ h2))
-
--------------------------------------------------------------------------------------------------------
+    h1 = ph group a1 m1 (depthUpdate d)
+    h2 = ph group m1 a2 (depthUpdate d)
+    h = V.concat [h1, h2]
+    hpar = h1 `par` (h2 `pseq` V.concat [h1, h2]) 
 
 {-Function to find the V2 with the maximum x-coordinate-}
 maxv :: VV2 -> V2
@@ -53,44 +49,48 @@ maxv = V.maximumBy (comparing fst)
 minv :: VV2 -> V2
 minv = V.minimumBy (comparing fst)
 
---------------------------------------------------------------------------------------------------------
-
 {-Furthest point from a line-}
 maxAreaPoint :: V2 -> V2 -> VV2 -> V2
-maxAreaPoint _ anchor2 points | V.null points = anchor2
+maxAreaPoint _ _ points | V.null points = error "missing points"
 maxAreaPoint anchor1 anchor2 points = V.maximumBy (comparing (triArea anchor1 anchor2)) points
 
+
+{-
+maxAreaPoint :: V2 -> V2 -> VV2 -> V2
+maxAreaPoint _ anchor2 points | V.null points = anchor2
+maxAreaPoint anchor1 anchor2 points = V.maximumBy (comparing (triArea anchor1 anchor2)) points  -}
+
 {-Groups by determinant (left, right)-}
-grouper :: V2 -> V2 -> VV2 -> (VV2, VV2)
-grouper anchor1 anchor2 points = (leftGroup, rightGroup)
+grouper :: V2 -> V2 -> VV2 -> VV2
+grouper anchor1 anchor2 points = leftGroup
   where
     leftGroup = V.filter (\z -> determinant anchor1 anchor2 z > 0) points
-    rightGroup = V.filter (\z -> determinant anchor1 anchor2 z < 0) points
+    --rightGroup = V.filter (\z -> determinant anchor1 anchor2 z < 0) points
 
 
 {-Calculates determinant-}
 determinant :: V2 -> V2 -> V2 -> Double
 determinant anchor1 anchor2 point = (w1 - z1) * (u2 - z2) - (w2 - z2) * (u1 - z1)
   where
-    z1 = fst anchor1 
-    z2 = snd anchor1 
-    w1 = fst anchor2 
-    w2 = snd anchor2 
-    u1 = fst point 
-    u2 = snd point 
+    z1 = fst anchor1
+    z2 = snd anchor1
+    w1 = fst anchor2
+    w2 = snd anchor2
+    u1 = fst point
+    u2 = snd point
 
 {-Area for triangle-}
 triArea :: V2 -> V2 -> V2 -> Double
 triArea v1 v2 v3 =
-    let x1 = fst v1 
-        y1 = snd v1 
-        x2 = fst v2 
-        y2 = snd v2 
-        x3 = fst v3 
-        y3 = snd v3 
+    let x1 = fst v1
+        y1 = snd v1
+        x2 = fst v2
+        y2 = snd v2
+        x3 = fst v3
+        y3 = snd v3
     in abs ((x1 * (y2 - y3)) + (x2 * (y3 - y1)) + (x3 * (y1 - y2)))
 
-depthUpdate :: Int -> Int 
-depthUpdate d 
-  | d == 1 || d == 0 = 0
+depthUpdate :: Int -> Int
+depthUpdate d
+  | d <= 1 = 0
   | otherwise = div d 2
